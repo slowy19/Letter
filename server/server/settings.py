b39a84b25833ad1d353a4cfa2af4786a5e3da383
slowy19 <135140649+slowy19@users.noreply.github.com>
2024-03-21 18:05:37 +0300

    client

--- server/server/settings.py
+++ server/server/settings.py
@@ -51,7 +51,7 @@ CORS_ALLOWED_ORIGINS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.common.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
