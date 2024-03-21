61c269e272158309667aa83efcdff6fec5cd9e25
Александр <poni.taw@yandex.ru>
2024-03-21 19:34:48 +0300

    Create chats, accounts

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('/login', include('accounts.urls')),
+    path('/signup', include('accounts.urls')),
+
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
 ]
