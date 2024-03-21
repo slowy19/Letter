aef01a70385e74c7d9d15bd181a0a7d88911a67a
slowy19 <135140649+slowy19@users.noreply.github.com>
2024-03-21 20:12:26 +0300

    update

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,19 +1,26 @@
+import Chats from '@/views/Chats.vue'
+import Login from '@/views/Login.vue'
+import Signup from '@/views/Signup.vue'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/signup',
+    name: 'signup',
+    component: Signup
+  },
+  {
+    path: '/chats',
+    name: 'chats',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: Chats
   }
 ]
 
---
+++ client/src/views/Chats.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+      Chats
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  
+  // @Options({
+  //   components: {
+  //     HelloWorld,
+  //   },
+  // })
+  export default class Chats extends Vue {}
+  </script>
+  
