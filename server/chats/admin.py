b45eb398c6db9babd4242f5372d5d72b75472f32
Александр <poni.taw@yandex.ru>
2024-03-23 12:04:01 +0300

    Update
    create new models in chat

--- server/chats/admin.py
+++ server/chats/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .models import Chat
 
 # Register the CustomUserAdmin class with the admin site
+@admin.register(Chat)
+class YourModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
